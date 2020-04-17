637ff8bc0e21bcfd0dbd82a64da6981f1e24d2af
Denis Cornehl <denis.cornehl@gmail.com>
2020-04-17 09:33:38 +0200

    add basic sentry support

--- config.py
+++ config.py
@@ -3,3 +3,10 @@ import os
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 PERSONIO_CLIENT_ID = os.environ.get("PERSONIO_CLIENT_ID")
 PERSONIO_CLIENT_SECRET = os.environ.get("PERSONIO_CLIENT_SECRET")
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+
+
+if SENTRY_DSN:
+    import sentry_sdk
+
+    sentry_sdk.init(SENTRY_DSN)
